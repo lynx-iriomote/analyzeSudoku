5ba8b679ffdfde20f8fad13c099205934c6e9834
lynx-iriomote <tomthemarble@gmail.com>
2019-06-29 17:00:14 +0900

    コメント支援機能追加

--- server/sudokuapp/data/AnalyzeWk.py
+++ server/sudokuapp/data/AnalyzeWk.py
@@ -1,4 +1,5 @@
 import dataclasses
+import os
 from typing import Dict, List
 
 from sudokuapp.const.Method import Method
@@ -104,3 +105,63 @@ class AnalyzeWk():
 
         self.histroy_list.append(
             History(self.flame.clone(), how_to_list))
+
+    def print_flame_attention(self, attention_num: int) -> None:
+        """ある数字に注目した枠の文字列をprintする
+
+        debug用
+            +[1]-------------------------+[2]-- ...
+            | 1:1      1:2      1:3      |      ...
+            | ?        m=[N,?]  m=[N,?]  |      ...
+            | 2:1      2:2      2:3      |      ...
+            | val=N    ?        m=[N,?]  |      ...
+            | 3:1      3:2      3:3      |      ...
+            | hint=N   ?        m=[N,?]  |      ...
+            +[4]-------------------------+[5]-- ...
+            ...
+
+        Args:
+            attention_num (int): 注目する数字
+        """
+        # TODO: 環境変数などを参照して開発時にしか出力しないようにする制御を入れる
+        flame_str_list: List[str] = list()
+        for loop_idx, squ_list in enumerate(self.row_dict.values()):
+            if loop_idx % 3 == 0:
+                flame_str_list.append(
+                    "+[{}]-------------------------+[{}]-------------------------+[{}]-------------------------+"
+                    .format(squ_list[0].area_id, squ_list[3].area_id, squ_list[6].area_id))
+            flame_str_list.append(
+                "| {}:{}      {}:{}      {}:{}      | {}:{}      {}:{}      {}:{}      | {}:{}      {}:{}      {}:{}      |"
+                .format(
+                    squ_list[0].row, squ_list[0].clm,
+                    squ_list[1].row, squ_list[1].clm,
+                    squ_list[2].row, squ_list[2].clm,
+                    squ_list[3].row, squ_list[3].clm,
+                    squ_list[4].row, squ_list[4].clm,
+                    squ_list[5].row, squ_list[5].clm,
+                    squ_list[6].row, squ_list[6].clm,
+                    squ_list[7].row, squ_list[7].clm,
+                    squ_list[8].row, squ_list[8].clm,
+                ))
+            row_str: str = ""
+            for squ_idx, squ in enumerate(squ_list):
+                if squ_idx % 3 == 0:
+                    row_str += "|"
+                row_str += " "
+
+                if squ.val == attention_num:
+                    row_str += "val={}".format(squ.val).ljust(8)
+                elif squ.hint_val == attention_num:
+                    row_str += "hint={}".format(squ.hint_val).ljust(8)
+                elif attention_num in squ.memo_val_list:
+                    row_str += "m=[{},?]".format(attention_num).ljust(8)
+                else:
+                    row_str += "?".ljust(8)
+
+                if squ_idx % 3 == 2:
+                    row_str += " "
+            row_str += "|"
+            flame_str_list.append(row_str)
+        flame_str_list.append(
+            "+----------------------------+----------------------------+----------------------------+")
+        print(os.linesep.join(flame_str_list))
